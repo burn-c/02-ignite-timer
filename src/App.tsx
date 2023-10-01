776f5785b6df914068b6fdea9e3087d8849af722
Carlos Oliveira <carlos.burndev@gmail.com>
2023-09-30 23:41:40 -0300

    feat(comp): adds Button test with styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { Button } from "./Button";
+
 export default function App() {
   return (
-    <h1>Ignite 2</h1>
+    <>
+      <h1>Ignite 2</h1>
+      <Button variant="primary"/>
+      <Button variant="secondary"/>
+      <Button variant="success"/>
+      <Button variant="danger"/>
+    </>
   )
 }
