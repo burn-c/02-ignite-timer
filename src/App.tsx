1744975b9f9e598165e6659a009ac59f856f3bbd
Carlos Oliveira <carlos.burndev@gmail.com>
2023-10-01 00:33:07 -0300

    lib: adds react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,18 @@
 import { ThemeProvider } from 'styled-components'
 
 import { defaultTheme } from './styles/theme/default'
-
-import { Button } from './Button'
 import { GlobalStyle } from './styles/global'
+import { Router } from './Router'
+import { BrowserRouter } from 'react-router-dom'
 
 export default function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
 
-      <h1>Ignite 2</h1>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="success" />
-      <Button variant="danger" />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
