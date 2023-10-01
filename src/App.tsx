1af8182548f8c6e051169778b0ffe453e4596cf9
Carlos Oliveira <carlos.burndev@gmail.com>
2023-09-30 23:58:10 -0300

    style: adds GlobalStyle

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,13 @@ import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/theme/default";
 
 import { Button } from "./Button";
+import { GlobalStyle } from "./styles/global";
 
 export default function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle/>
+      
       <h1>Ignite 2</h1>
       <Button variant="primary"/>
       <Button variant="secondary"/>
