8ee295a5c105c1b0cd18ce118f8f7301125e7bcf
Carlos Oliveira <carlos.burndev@gmail.com>
2023-09-30 23:47:38 -0300

    style: adds defaultTheme

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
+import { ThemeProvider } from "styled-components";
+
+import { defaultTheme } from "./styles/theme/default";
+
 import { Button } from "./Button";
 
 export default function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <h1>Ignite 2</h1>
       <Button variant="primary"/>
       <Button variant="secondary"/>
       <Button variant="success"/>
       <Button variant="danger"/>
-    </>
+    </ThemeProvider>
   )
 }
