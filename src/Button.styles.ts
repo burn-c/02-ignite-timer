60adf3bb65d9186dc4cd10026b153592e3c99085
Carlos Oliveira <carlos.burndev@gmail.com>
2023-10-01 00:07:36 -0300

    style: adds colors and fonts

--- src/Button.styles.ts
+++ src/Button.styles.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export type ButtonVariants = 'primary' | 'secondary' | 'danger' | 'success';
 
@@ -17,7 +17,11 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
 
-  ${props => {
-    return `background-color: ${buttonVariants[props.variant]}`
-  }}
+  background-color: ${props => props.theme["green-500"]};
+  
+  /* ${props => {
+    return css`
+      background-color: ${buttonVariants[props.variant]}
+    `
+  }} */
 `
